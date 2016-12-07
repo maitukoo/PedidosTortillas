2d5bf4a68ba3d5635f03bef041274a46c1b22fde
maitukoo <maitukoo@gmail.com>
2016-12-07 14:21:32 +0100

    pruebas Spinner

---
+++ app/src/main/java/com/example/hibryd/pedidostortillas/TortillaRow.java
@@ -0,0 +1,16 @@
+package com.example.hibryd.pedidostortillas;
+
+/**
+ * Created by adminportatil on 07/12/2016.
+ */
+
+public class TortillaRow {
+        int imagen;
+        String nombre;
+
+        public void setData(String nombre, String place, int imagen) {
+            this.nombre = nombre;
+            this.imagen = imagen;
+        }
+
+}
