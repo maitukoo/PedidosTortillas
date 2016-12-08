81cb3f7284f432aae093980340cc8a2d4401215a
Hibryd <maitukoo@gmail.com>
2016-12-08 08:45:12 +0100

    Primera pantalla

---
+++ app/src/main/java/com/example/hibryd/pedidostortillas/Cliente.java
@@ -0,0 +1,34 @@
+package com.example.hibryd.pedidostortillas;
+
+/**
+ * Created by Hibryd on 06/12/2016.
+ */
+
+public class Cliente {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
