4ba2086962cea2f1d2c3128769589f69f1a8f938
Hibryd <maitukoo@gmail.com>
2016-12-11 19:24:45 +0100

    Documentacion y cambios en pantalla eligeTortillas

--- app/src/main/java/com/example/hibryd/pedidostortillas/Tortilla.java
+++ app/src/main/java/com/example/hibryd/pedidostortillas/Tortilla.java
@@ -5,5 +5,43 @@ package com.example.hibryd.pedidostortillas;
  */
 
 public class Tortilla {
+
+    private int numeroTortillas;
     private String tipoTortilla;
+    private String tipoHuevos;
+    private String tamanio;
+
+    public int getNumeroTortillas() {
+        return numeroTortillas;
+    }
+
+    public void setNumeroTortillas(int numeroTortillas) {
+        this.numeroTortillas = numeroTortillas;
+    }
+
+    public String getTipoTortilla() {
+        return tipoTortilla;
+    }
+
+    public void setTipoTortilla(String tipoTortilla) {
+        this.tipoTortilla = tipoTortilla;
+    }
+
+    public String getTipoHuevos() {
+        return tipoHuevos;
+    }
+
+    public void setTipoHuevos(String tipoHuevos) {
+        this.tipoHuevos = tipoHuevos;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+
 }
