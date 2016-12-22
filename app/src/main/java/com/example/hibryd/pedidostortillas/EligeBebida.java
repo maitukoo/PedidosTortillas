b3a303cc3d06c69691f6850f8448312c2c94d127
scarfre <Tortilla1>
2016-12-22 09:49:01 +0100

    He testeao segun he descargado lo tuyo y da un fallo al pasar de actividad, pero no consigo encontrar en que puede estar el error. Es algo de los spinners. Aqui subo lo que ma dao tiempo de hacer en clase con lo que tarda esta mierda xD

--- app/src/main/java/com/example/hibryd/pedidostortillas/EligeBebida.java
+++ app/src/main/java/com/example/hibryd/pedidostortillas/EligeBebida.java
@@ -6,18 +6,77 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Created by adminportatil on 07/12/2016.
  */
 
 public class EligeBebida extends AppCompatActivity{
-    private String [] tipoBebida={"Coca cola", "Limón", "Naranja", "Nestea", "cerveza", "Agua"};
-    private Spinner spin;
+    private Bebida bebida;
+    private Datos datos;
+    private Button botonAniadir;
+    private CheckBox checkCola;
+    private CheckBox checkLimon;
+    private CheckBox checkNaranja;
+    private CheckBox checkNestea;
+    private CheckBox checkCerveza;
+    private CheckBox checkAgua;
+    private EditText cantidadCola;
+    private EditText cantidadLimon;
+    private EditText cantidadNaranja;
+    private EditText cantidadNestea;
+    private EditText cantidadCerveza;
+    private EditText cantidadAgua;
+    private ArrayList<Datos> arrayParametros = new ArrayList<Datos>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.eligebebida);
+
+        botonAniadir=(Button) findViewById(R.id.btnAñadirBebidas);
+        checkCola=(CheckBox) findViewById(R.id.ckbCola);
+        checkLimon=(CheckBox) findViewById(R.id.ckbLimon);
+        checkNaranja=(CheckBox)findViewById(R.id.ckbNaranja);
+        checkNestea=(CheckBox) findViewById(R.id.ckbNestea);
+        checkCerveza=(CheckBox) findViewById(R.id.cerveza);
+        checkAgua=(CheckBox) findViewById(R.id.ckbAgua);
+        cantidadCola=(EditText) findViewById(R.id.txtCantidadCola);
+        cantidadLimon=(EditText) findViewById(R.id.txtCantidadLimon);
+        cantidadNaranja=(EditText) findViewById(R.id.txtCantidadnaranja);
+        cantidadNestea=(EditText) findViewById(R.id.txtCantidadNestea);
+        cantidadCerveza=(EditText) findViewById(R.id.txtCantidadCerveza);
+        cantidadAgua=(EditText) findViewById(R.id.txtCantidadAgua);
+
+        checkCola.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (checkCola.isChecked()){
+                    cantidadCola.setEnabled(true);
+                }
+                else{
+                    cantidadCola.setText("");
+                    cantidadCola.setEnabled(false);
+                }
+            }
+        });
+        checkLimon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkLimon.isChecked()){
+                    cantidadLimon.setEnabled(true);
+                }
+                else{
+                    cantidadCola.setText("");
+                    cantidadLimon.setEnabled(false);
+                }
+            }
+        });
     }
 }
